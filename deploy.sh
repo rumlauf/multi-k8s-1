1c96e321ae292654018b2d70f30f4b567e473be7
rumlauf <rumlauf@outlook.com>
2020-11-21 18:25:21 -0300

    trocado usuario

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t rumlauf/multi-client:latest -t rumlauf/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rumlauf/multi-server:latest -t rumlauf/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rumlauf/multi-worker:latest -t rumlauf/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push rumlauf/multi-client:latest
+docker push rumlauf/multi-server:latest
+docker push rumlauf/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push rumlauf/multi-client:$SHA
+docker push rumlauf/multi-server:$SHA
+docker push rumlauf/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=rumlauf/multi-server:$SHA
+kubectl set image deployments/client-deployment client=rumlauf/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rumlauf/multi-worker:$SHA
